d3d1c08f473dc63fa28583acda255adb7ce4fa07
rbmm <_dm_sh_@tutanota.com>
2024-08-24 14:19:30 +0300

    Update x64.asm

--- SCx64/x64.asm
+++ SCx64/x64.asm
@@ -8,7 +8,7 @@ extern ?ep@@YAXPEAU_PEB@@@Z : PROC
 	jmp ?ep@@YAXPEAU_PEB@@@Z
 ?epASM@@YAXPEAU_PEB@@@Z endp
 
-include <..\common\nobase64.inc>
+include <..\scentry\nobase64.inc>
 
 include <imp.asm>
 
